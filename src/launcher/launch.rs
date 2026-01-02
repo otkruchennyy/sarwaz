9348a49e59d1c992bb6aaaae999c3a094a9a23ce
tsukagahara <tsukagaharra@mail.ru>
2026-01-02 03:00:40 +0300

    Reracttor application

--- src/launcher/launch.rs
+++ src/launcher/launch.rs
@@ -1,24 +1,29 @@
-use iced::widget::{column, Column};
+use iced::widget::{button, row, text, Row};
+use iced::{window, Size};
 
 pub fn launcher() -> iced::Result {
-    iced::application(init, update, view).run()
+    iced::application(init, update, view)
+        .window(window::Settings {
+            decorations: false,
+            transparent: true,
+            size: Size::new(400.0, 200.0),
+            ..window::Settings::default()
+        })
+        .title("Sarwaz launcher") // ← Заголовок задаётся здесь!
+        .run()
 }
 
-// 1. Функция инициализации (заменяет `new`)
 fn init() -> (u64, iced::Task<Message>) {
-    (0, iced::Task::none()) // Возвращаем начальное состояние и фоновую задачу
+    (0, iced::Task::none())
 }
 
-// 2. Перечисление сообщений
 #[derive(Debug, Clone)]
 enum Message {}
 
-// 3. Функция обновления (заменяет `update`)
 fn update(state: &mut u64, message: Message) {
     match message {}
 }
 
-// 4. Функция отображения (заменяет `view`)
-fn view(state: &u64) -> Column<Message> {
-    column![]
+fn view(_state: &u64) -> Row<Message> {
+    row![text("launcher")]
 }
